3afb72524b4a988734a1f401577ac9ce32c01e4b
Catlinman <andrew.lake@amwasser.de>
2014-05-23 23:08:18 +0200

    Added first collision detection functions and a fluid collider class

--- fluidsystem.lua
+++ fluidsystem.lua
@@ -35,8 +35,8 @@ fluidsystem = {} -- Global variable containing the functions used to create and 
 function fluidsystem.new()
 	local system = {}
 
-	-- This value defaults to 9.81 since the system is intended for sidescrolling games. A value of zero might be useful for top down based games.
-	system.gravity = 9.81
+	-- This value defaults to 0.981 since the system is intended for sidescrolling games. A value of zero might be useful for top down based games.
+	system.gravity = 0.981
 
 	system.damping = 1 -- How much particles lose velocity when not colliding
 
@@ -51,7 +51,7 @@ function fluidsystem.new()
 	system.affectors = {} -- Table containing objects that affect the flow of particles
 
 	-- Add and remove particles using the following two methods
-	function system:addParticle(x, y, vx, vy, color, size)
+	function system:addParticle(x, y, vx, vy, color, r)
 		local particle = {} -- Create a new particle contained in a table
 
 		-- Assign values that we will use to track certain states of the particle
@@ -62,9 +62,12 @@ function fluidsystem.new()
 		particle.vx = vx or 0
 		particle.vy = vy or 0
 
-		-- Color and size
+		-- Color, radius and collider
 		particle.color = color or {255, 255, 255, 255} -- Colors: {RED, GREEN, BLUE, ALPHA/OPACITY}
+		particle.r = r or 1
+		particle.collider = fluidsystem.createCircleCollider(particle.r)
 
+		-- Id assignment
 		particle.id = self.particleId
 		self.particleId = self.particleId + 1
 
@@ -95,14 +98,35 @@ function fluidsystem.new()
 
 	-- Method to simulate a frame of the simulation. This is where the real deal takes place.
 	function system:simulate(dt)
+		for i, particle in pairs(self.particles) do
+			-- Add the system's gravity to the particles velocity
+			particle.vy = particle.vy + self.gravity
 
+			-- We apply each particles velocity to it's current position
+			particle.x = particle.x + particle.vx
+			particle.y = particle.y + particle.vy
+
+			-- Check if the particle is out of bounds and resolve the collision
+			if particle.y + particle.r > 768 then
+				particle.y = 768 - particle.r
+				particle.vy = -particle.vy / 2
+			end
+
+			-- Perform collision detection and resolution here
+			for j, particle2 in pairs(self.particles) do
+				-- Make sure we are not checking against an already checked particle
+				if particle2 ~= particle then
+					if particle.collider:circleCollision(particle2.collider) then print("Detected collision") end
+				end
+			end
+		end
 	end
 
 	-- Method to draw the current state of the fluid simulation
 	function system:draw()
 		for i, particle in pairs(self.particles) do
 			love.graphics.setColor(particle.color)
-			love.graphics.circle("fill", particle.x, particle.y, particle.size, 8)
+			love.graphics.circle("fill", particle.x, particle.y, particle.r, 16)
 		end
 
 		love.graphics.setColor(255, 255, 255, 255) -- We reset the global color so we don't affect any other game drawing events
@@ -129,4 +153,74 @@ function fluidsystem.destroy()
 
 		systems[id] = nil
 	end
+end
+
+-- Fluid system collider classes. The fluid system takes in any of these and uses them to interact with the particle system.
+
+-- Base collider class - only contains functions used to handle conversion between collider types.
+function fluidsystem.createBaseCollider(x, y)
+	local collider = {}
+
+	collider.x = x or 0
+	collider.y = y or 0
+
+	-- The 'c' argument is the collider that this one is to check for collision against
+
+	-- Basic box collision detection
+	function collider:boxCollision(c)
+		-- Convert this and the selected colliders types to those usable by box collision
+		local w = self.w or self.r or 16
+		local h = self.h or self.r or 16
+
+		local w2 = c.w or c.r or 16
+		local h2 = c.h or c.r or 16
+
+		local x2, y2, cx2, cy2 = self.x + w, self.y + h, c.x + w2, c.y + h2
+
+		-- Returns true if a box collision was detected
+		return self.x < cx2 and x2 > c.x and self.y < cy2 and y2 > c.y
+	end
+
+	-- Circle collision without the use of math.sqrt
+	function collider:circleCollision(c)
+		local r = self.w or self.r or 8
+		local r2 = c.w or c.r or 8
+
+		local dist = (c.x - self.x)^2 + (c.y - self.y)^2
+
+		-- Returns true if a circle collision was detected
+		return (dist + r2^2) < r^2
+	end
+
+	function collider:pixelCollision(c)
+		-- Still needs code
+	end
+
+	return collider
+end
+
+-- The most basic collider type. Simple box intersection collider.
+function fluidsystem.createBoxCollider(x, y, w, h)
+	local collider = fluidsystem.createBaseCollider(x, y)
+
+	collider.w = w or 16
+	collider.h = h or 16
+
+	return collider
+end
+
+-- Circle collider uses radius based calculation to detect and resolve collision
+function fluidsystem.createCircleCollider(x, y, r)
+	local collider = fluidsystem.createBaseCollider(x, y)
+
+	collider.r = r or 8
+
+	return collider
+end
+
+-- Image collider takes in an image to calculate pixel perfect collision
+function fluidsystem.createImageCollider(x, y, sx, sy, imagedata)
+	local collider = fluidsystem.createBaseCollider(x, y)
+
+	return collider
 end
--- main.lua
+++ main.lua
@@ -1,13 +1,17 @@
 require("fluidsystem")
 
-function love.load()
+local fluid = {}
 
+function love.load()
+	fluid = fluidsystem.new()
+	fluid:addParticle(32,32,0,0,nil,16)
+	fluid:addParticle(128,128,0,0,nil,16)
 end
 
 function love.update(dt)
-
+	fluid:simulate(dt)
 end
 
 function love.draw()
-
+	fluid:draw()
 end
